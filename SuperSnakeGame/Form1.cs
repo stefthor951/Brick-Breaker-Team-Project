0e2f412b3b09d8ccb3d9c55a1914abf1644afcc1
dylolemu <dylolemu853@ed.amdsb.ca>
2017-04-24 14:45:21 -0400

    Add project files.

---
+++ SuperSnakeGame/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using BrickBreaker.Screens;
+
+namespace BrickBreaker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Start the program centred on the Menu Screen
+            MenuScreen ps = new MenuScreen();
+            this.Controls.Add(ps);
+
+            ps.Location = new Point((this.Width - ps.Width) / 2, (this.Height - ps.Height) / 2);
+        }
+    }
+}
