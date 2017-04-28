a4929ca7e9cdd0c87f33af88330029ef954eecad
logaashb <logaashb222@ed.amdsb.ca>
2017-04-28 14:37:06 -0400

    hj

--- SuperSnakeGame/Screens/LoseScreen.cs
+++ SuperSnakeGame/Screens/LoseScreen.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace BrickBreaker
+namespace BrickBreaker 
 {
     public partial class LoseScreen : UserControl
     {
